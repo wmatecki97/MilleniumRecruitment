8a1b1b9f6fd272dcfecc238ca6993a0377785f5e
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2022-08-24 11:54:47 +0200

    unit tests project added

---
+++ MilleniumRecruitment.Tests/AnimalControllerTests.cs
@@ -0,0 +1,14 @@
+using MilleniumRecruitment.Controllers;
+
+namespace MilleniumRecruitment.Tests
+{
+    public class AnimalControllerTests
+    {
+
+        [Test]
+        public void Test1()
+        {
+            var controller = new AnimalController()
+        }
+    }
+}
