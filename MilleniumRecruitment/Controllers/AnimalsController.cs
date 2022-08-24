3483f14ed73637794968a3f2f59ebf2ef28a53a9
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2022-08-24 10:32:13 +0200

    Add project files.

---
+++ MilleniumRecruitment/Controllers/AnimalsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MilleniumRecruitment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimalsController : ControllerBase
+    {
+    }
+}
